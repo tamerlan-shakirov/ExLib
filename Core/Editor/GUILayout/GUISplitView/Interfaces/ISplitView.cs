32089b3dcdbc524b24bb0ee14dd700894fc9e4bf
Tamerlan <tamerlan.shakirov@yandex.ru>
2022-10-28 13:44:58 +0500

    add gui split view

---
+++ Core/Editor/GUILayout/GUISplitView/Interfaces/ISplitView.cs
@@ -0,0 +1,20 @@
+﻿/* ================================================================
+   ----------------------------------------------------------------
+   Project   :   ExLib
+   Company   :   Renowned Games
+   Developer :   Tamerlan Shakirov
+   ----------------------------------------------------------------
+   Copyright 2022 Tamerlan Shakirov All rights reserved.
+   ================================================================ */
+
+namespace RenownedGames.ExLibEditor
+{
+    public interface ISplitView
+    {
+        void BeginSplitView();
+
+        void Split();
+
+        void EndSplitView();
+    }
+}
